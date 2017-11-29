495cc931656e93da72a06f0c9f7f0e362fda6d25
Administrator <Administrator@192.168.100.56>
2017-11-29 20:07:20 +0900

    index.jsp charset 수정 and login web ect. 수정

--- 20171123/WebContent/login.jsp
+++ 20171123/WebContent/login.jsp
@@ -9,29 +9,28 @@
 <body>
 
 <script>
-function checkValue1(){
-	var id = document.getElementById("id");
-	var pwd = document.getElementById("pwd");
-	if (id.value.trim()=="") {
-		alert("id를 입력해주세요");
+function checkVlaue(){ 
+	var id=document.getElementById("id").value; 
+	var pwd=document.getElementById("pwd").value;
+	//window.alert("id="+id+",pwd="+pwd);
+	if(id.trim()==""){ //if(id=="")로 하면, 공백을 한개초과입력했을떄 비밀번호로 넘어가진까 안됌! trim(=공백지우기)붙여주기.
+		alert("ID를 입력해주세요.");
 		id.focus();
-		return false;
+		return false; 
 	}
-	if (pwd.value.trim()=="") {
-		alert("pw를 입력해주세요");
+	if(pwd.trim()==""){//위에getElementById여기다가 .value 안하면, 여기 공백확인 if문에 해줘야함.
+		alert("비밀번호를 입력해주세요.");
 		pwd.focus();
 		return false;
 	}
 	return true;
-}		
+}
 </script>
-
-<form method ="Post" action ="/test.login" onsubmit="return checkValue1()">
-아이디 : <input type = "text" name = "id" id = "id"><br>
-비밀번호 : <input type = "password" name = "pwd" id = "pwd"><br>
-<input type="submit" value = "login">
+<form method="Post" action="/test.login" onsubmit="return checkVlaue()"><br>
+아이디 : <input type="text" name="id" id="id">
+비밀번호 : <input type="password" name="pwd" id="pwd" >
+<input type="submit" value="login">
 </form>
-
 
 
 </body>
--- 20171123/src/com/test/jsp/servlet/LoginServlet.java
+++ 20171123/src/com/test/jsp/servlet/LoginServlet.java
@@ -1,4 +1,40 @@
-package com.test.jsp.servlet;//WebContent(=/)의 login.jsp(java내용 지운거)랑 세트임
+//package com.test.jsp.servlet;//WebContent(=/)의 login.jsp(java내용 지운거)랑 세트임
+//
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//public class LoginServlet extends HttpServlet{
+//	//.jsp파일에서 method="get"으로 해야 함.
+//	public void doGet(HttpServletRequest req,HttpServletResponse res)throws
+//	IOException, ServletException{
+//		req.setCharacterEncoding("UTF-8");
+//		String id=req.getParameter("id");
+//		String pwd=req.getParameter("pwd");
+//		System.out.println("로그인 서블릿 호출!");
+//		res.setContentType("text/html; charset=UTF-8");
+//		PrintWriter pw=res.getWriter();
+//		pw.print("입력하신 id="+id); 
+//		pw.print("입력하신 pwd="+pwd);
+//	}
+//	//.jsp파일에서 method="post"로 해야 함.
+//	public void doPost(HttpServletRequest req,HttpServletResponse res)throws
+//	IOException, ServletException{
+//		req.setCharacterEncoding("UTF-8");
+//		String id=req.getParameter("id");
+//		String pwd=req.getParameter("pwd");
+//		System.out.println("로그인 서블릿 호출!");
+//		res.setContentType("text/html; charset=UTF-8");
+//		PrintWriter pw=res.getWriter();
+//		pw.print("포스트로 날 호출 했군요."); 
+//	}
+//}
+
+package com.test.jsp.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,28 +44,37 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class LoginServlet extends HttpServlet{
-	//.jsp파일에서 method="get"으로 해야 함.
-	public void doGet(HttpServletRequest req,HttpServletResponse res)throws
-	IOException, ServletException{
-		req.setCharacterEncoding("UTF-8");
-		String id=req.getParameter("id");
-		String pwd=req.getParameter("pwd");
-		System.out.println("로그인 서블릿 호출!");
-		res.setContentType("text/html; charset=UTF-8");
-		PrintWriter pw=res.getWriter();
-		pw.print("입력하신 id="+id); 
-		pw.print("입력하신 pwd="+pwd);
+public class LoginServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws 
+	IOException, ServletException {
+		req.setCharacterEncoding("utf-8");
+		
+		String id = req.getParameter("id");
+		String pwd = req.getParameter("pwd");
+
+		System.out.println("로그인 서블릿 호출 !!");
+		res.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = res.getWriter();
+		pw.print("입력하신 id = " + id);
+		pw.print("입력하신 pwd = " + pwd);
+
 	}
-	//.jsp파일에서 method="post"로 해야 함.
-	public void doPost(HttpServletRequest req,HttpServletResponse res)throws
-	IOException, ServletException{
-		req.setCharacterEncoding("UTF-8");
-		String id=req.getParameter("id");
-		String pwd=req.getParameter("pwd");
-		System.out.println("로그인 서블릿 호출!");
-		res.setContentType("text/html; charset=UTF-8");
-		PrintWriter pw=res.getWriter();
-		pw.print("포스트로 날 호출 했군요."); 
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws 
+	IOException, ServletException {
+		req.setCharacterEncoding("utf-8");
+		
+		String id = req.getParameter("id");
+		String pwd = req.getParameter("pwd");
+
+		System.out.println("로그인 서블릿 호출 !!");
+		res.setContentType("text/html;charset=utf-8");
+		PrintWriter pw = res.getWriter();
+		//System.out.println("doPost로 날 불렀네요");
+		pw.print("doPost로 날 불렀네요");
+
 	}
+
 }
+
